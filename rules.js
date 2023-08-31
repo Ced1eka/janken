06727714e8307c38b1da896eb229144f0f58796b
Ced <cedieka2@gmail.com>
2023-09-01 03:08:58 +0900

    Added a function for one round of the rps game.
    Made a nested if else statement for said rule.

--- rules.js
+++ rules.js
@@ -7,19 +7,45 @@ let computerChoice = choice[Math.floor(Math.random() * choice.length)];
 
 return computerChoice;
 }
-// console.log(getComputerChoice());
+
+
 
 // get player choice
 
-function playerTurn (){
+// function playerTurn (){
 
-      let Player = prompt("your play!");
+//       let Player = prompt("your play!");
 
-      const lowerCased = Player.toLowerCase();
-      const firstUpper = lowerCased.toUpperCase()[0]; //Capitalizes first letter
-      const modded = lowerCased.replace(lowerCased[0], `${firstUpper}`);
-      return modded;
+//       let lowerCased = Player.toLowerCase();
+//       let firstUpper = lowerCased.toUpperCase()[0]; //Capitalizes first letter
+//       let modded = lowerCased.replace(lowerCased[0], `${firstUpper}`);
+//       return modded;
+
+// }
+
+let Player = prompt("your play!");
+let lowerCased = Player.toLowerCase();
+let firstUpper = lowerCased.toUpperCase()[0]; //Capitalizes first letter
+const playerSelection = lowerCased.replace(lowerCased[0], `${firstUpper}`);
+const computerSelection = getComputerChoice();
+
+function playRound(playerSelection, computerSelection) {
+      // your code here!
+      if(playerSelection === computerSelection){
+            alert('draw');
+      } else if (playerSelection === "Rock" && computerSelection === "Paper"||
+                  playerSelection === "Paper" && computerSelection === "Scissors" ||
+                  playerSelection === "Scissors" && computerSelection === "Rock"){
+            alert('computer won');
+      } else if (playerSelection === "Paper" && computerSelection === "Rock"||
+      playerSelection === "Scissors" && computerSelection === "Paper" ||
+      playerSelection === "Rock" && computerSelection === "Scissors"){
+            alert('player won');
+      } else {
+            alert("this isn't an anime");
+      }
 }
-// letsPlay();
+playRound(playerSelection,computerSelection);
 
-console.log(playerTurn());
+    console.log(playerSelection, computerSelection);
+//     console.log(playRound(playerSelection, computerSelection));
